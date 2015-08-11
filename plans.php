08a2e1b9fa75435d241eadfdf4243d53713ec6b0
CJ Dvorak <cjdvorak@starverte.com>
2015-08-11 11:59:38 -0600

    Started to make the /plan/$ page pretty

--- plans.php
+++ plans.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-Type: application/json');
 
 include_once('config.php');
 include_once('inc/class-fccdb.php');
@@ -15,19 +14,60 @@ else {
   $method = 'get';
 }
 if ( !empty( $_REQUEST['id']  ) ) {
-  $plan_id = $_REQUEST['id'];
+  $plan_id = (int) $_REQUEST['id'];
 }
 else {
   $plan_id = rand(1,25);
 }
 
-$plan_id = min($plan_id,25);
-$plan_id = max($plan_id,0);
-
 switch ($method) {
   case 'get' :
     $plan = Plan::get_instance( $plan_id );
-    echo json_encode($plan, JSON_PRETTY_PRINT);
+    if (!$_REQUEST['raw']) {
+      ?><!DOCTYPE html>
+      <html>
+      <head>
+        <title><?php echo "The $plan->plan_name Plan - Star Verte";?></title>
+        <style type="text/css">
+          div {
+            position: relative;
+            width: 100%;
+            margin-top: 0px;
+            margin-bottom: 0px;
+            margin-left: 0px;
+            margin-right: 0px;
+            padding-left: 70px;
+            padding-right: 70px;
+            font-family: 'Open Sans', sans-serif;
+          }
+          #header {
+            background-color: rgb(248, 248, 248);
+            padding-top: 20px;
+            padding-bottom: 20px;
+          }
+          #title {
+            background-color: rgb(60, 180, 0);
+            font-size: 36px;
+            color: #fff;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="header"><img src="http://starverte.com/wp-content/uploads/2014/08/Star-Verte-small1.png" width="291" height="55"></div>
+        <div id="title">This is the <?php echo $plan->plan_name ?> Plan from Star Verte</div>
+        <div id="info">This plan includes <?php echo $plan->plan_storage ?>GB of storage, <?php echo $plan->plan_installs ?> separate Wordpress installs, for <?php 
+          if (!empty($plan->plan_price_monthly)) {echo "$$plan->plan_price_monthly a month";}
+          if (!empty($plan->plan_price_monthly) && !empty($plan->plan_price_yearly)) {echo ", or ";}
+          if (!empty($plan->plan_price_yearly)) {echo "$$plan->plan_price_yearly a year";}
+          if (empty($plan->plan_price_monthly) && empty($plan->plan_price_yearly)) {echo "an unknown amount of money (see <a href='../plan/$plan_id?raw=1'>here</a> for more info)";}
+         ?></div>
+      </body>
+      </html>
+      <?php
+    }
+    else {
+      echo json_encode($plan, JSON_PRETTY_PRINT);
+    }
     break;
   default:
     break;
